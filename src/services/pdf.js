5805c9166289c77bc6e6f12cbaf980d3ccb08fae
Isaac Pontes <isaaclfp@outlook.com>
2022-08-30 12:52:54 -0300

    fix: add brasao to pdf reports

--- src/services/pdf.js
+++ src/services/pdf.js
@@ -4,10 +4,11 @@ import autotable from 'jspdf-autotable'
 export function generatePdfReport({ title, description, thead, tbody, filename }) {
   const doc = new jsPDF({ orientation: 'landscape' })
   doc.setFontSize(12)
-  doc.addImage('/logo.png', 'png', 14, 11)
-  doc.text('Programa Fidelense Cidadão', 48, 16)
-  doc.text(title, 48, 22)
-  doc.text(description, 48, 28)
+  doc.addImage('/brasao.png', 'png', 14, 12)
+  doc.addImage('/logo.png', 'png', 253, 11)
+  doc.text('Programa Fidelense Cidadão', 36, 16)
+  doc.text(title, 36, 22)
+  doc.text(description, 36, 28)
   doc.line(14, 32, 283, 32)
   autotable(doc, {
     head: thead,
